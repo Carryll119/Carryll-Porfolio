b07f7cc207bb698fc63d4d687a8ea907c820842c
Carryll119 <carryll119@gmail.com>
2025-06-01 21:12:23 +0800

    fix resume page

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,17 @@
 import {defineConfig} from "vite"
+import { resolve}	from "path"
 
 export default defineConfig({
+	build:{
+		outDir: "dist",
+		rolluptOptions:{
+			input:{
+				main: resolve(__dirname, "index.html"),
+				resume: resolve(__dirname, "resume.html"),
+			}
+		}
+
+	},
 	plugins: [
 		
 	]
